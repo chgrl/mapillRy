42679c4e6eb1316c3f481bbedd832d6710c67c0d
chgrl <christian.graul@gmail.com>
2015-11-09 20:16:57 +0100

    Mapillary image license added

--- R/mapillRy.R
+++ R/mapillRy.R
@@ -1,7 +1,10 @@
 #' @title R Interface to the Mapillary API
 #' @description A wrapper for the Mapillary API. 'mapillRy' gives access to images, 
-#' image sequences and stats of Mapillary, a service for crowdsourcing map images, 
-#' available at <https://www.mapillary.com>.
+#' image sequences and stats of Mapillary, a service for crowdsourcing map images: 
+#' <https://www.mapillary.com>.
+#' 
+#' Mapillary images are available under the Creative Commons Attribution-ShareAlike 4.0 
+#' International License (CC BY-SA 4.0).
 #' @name mapillRy
 #' @docType package
 #' @importFrom httr GET
